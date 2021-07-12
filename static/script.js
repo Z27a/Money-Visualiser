4f3c2925a585b78713c256e59dab06703241a8fd
Omni2002 <yagebuxie@gmail.com>
2021-07-12 22:18:12 +1000

    Yagebu's first commit

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// const nav_toggle = document.querySelector(".nav-toggle")
+// const navbar = document.querySelector(".navbar")
+
+// nav_toggle.addEventListener("change", navbar.style.display = "block");
+
+// document.querySelector('.navbar').style.color = "green";
