f46bfe17bbd4baf6ebe27fc364ffc2cc0b2fa2fa
Leo <leolin.360@gmail.com>
2021-07-14 10:32:47 +1000

    Added average daily spending to graph

--- static/graphs.js
+++ static/graphs.js
@@ -3,6 +3,11 @@ for (let i = 0; i < amounts.length; i++) {
   averages.push(1425);
 }
 
+var dailies = []
+for (let i = 0; i < amounts.length; i++) {
+  dailies.push(204);
+}
+
 const labels = goodDates;
 const data = {
     labels: labels,
@@ -16,6 +21,11 @@ const data = {
         backgroundColor: 'rgb(255, 138, 0)',
         borderColor: 'rgb(255, 138, 0)',
         data: averages,
+    }, {
+        label: 'Aus Average Daily Spending',
+        backgroundColor: 'rgb(232, 97, 12)',
+        borderColor: 'rgb(232, 97, 12)',
+        data: dailies,
     }]
 };
 
