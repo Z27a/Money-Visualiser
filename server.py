9a91d948bbabeb3a58a2013713ba667b725931a8
Omni2002 <yagebuxie@gmail.com>
2021-07-15 00:14:18 +1000

    add a separate save to history button in homepage

--- server.py
+++ server.py
@@ -13,22 +13,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Users(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	fullname = db.Column(db.String(50))
-	country = db.Column(db.String(50))
-	username = db.Column(db.String(100), unique=True, nullable=False)
-	password = db.Column(db.String(100))
-	s_goal = db.Column(db.Integer)
-	s_progress = db.Column(db.Integer)
-	history = db.relationship('History', backref='account_holder', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100))
+    s_goal = db.Column(db.Integer)
+    s_progress = db.Column(db.Integer)
+    history = db.relationship('History', backref='account_holder', lazy=True)
 
-	def __init__(self, fullname, country, username, password, s_goal, s_progress):
-		self.fullname = fullname
-		self.country = country
-		self.username = username
-		self.password = password
-		self.s_goal = s_goal
-		self.s_progress = s_progress
+    def __init__(self, fullname, country, username, password, s_goal, s_progress):
+        self.fullname = fullname
+        self.country = country
+        self.username = username
+        self.password = password
+        self.s_goal = s_goal
+        self.s_progress = s_progress
 
 class History(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,8 +39,8 @@ class History(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     # def __init__(self, _object, amount):
-    # 	self._object = _object
-    # 	self.amount = amount
+    #   self._object = _object
+    #   self.amount = amount
     def __repr__(self):
         return f"History('{self.title}', '{self.date}', '{self._object}', '{self.amount}')"
 
@@ -97,21 +97,24 @@ def login():
 
 @app.route("/signup", methods=['POST', 'GET'])
 def signup():
-	if request.method == 'POST':
-		fullname = request.form["fullname"]
-		country = request.form["country"]
-		user = request.form["username"]
-		pwd = request.form["password"]
-		usr = Users(fullname=fullname, country=country, username=user, password=pwd, s_goal=1, s_progress=0)
-		db.session.add(usr)
-		db.session.commit()
-		return redirect(url_for('login'))
-	else:
-		return render_template('signup.html')
+    if request.method == 'POST':
+        fullname = request.form["fullname"]
+        country = request.form["country"]
+        user = request.form["username"]
+        pwd = request.form["password"]
+        usr = Users(fullname=fullname, country=country, username=user, password=pwd, s_goal=1, s_progress=0)
+        db.session.add(usr)
+        db.session.commit()
+        return redirect(url_for('login'))
+    else:
+        return render_template('signup.html')
 
 @app.route("/<usrname>/<amt>", methods=['POST', 'GET'])
 def account_page(usrname, amt=0):
     if request.method == 'POST':
+        # go = request.form.get('go_btn')
+        save = request.form.get('save_btn')
+        
         user = usrname
         if request.form["title"]:
             title = request.form["title"]
@@ -119,9 +122,10 @@ def account_page(usrname, amt=0):
             title = 'Untitled'
         obj = request.form["object"]
         amount = request.form["money_amount"]
-        save1 = History(title=title, _object=obj, amount=amount, account_holder=Users.query.filter_by(username=usrname).first())
-        db.session.add(save1)
-        db.session.commit()
+        if save is not None:
+            save1 = History(title=title, _object=obj, amount=amount, account_holder=Users.query.filter_by(username=usrname).first())
+            db.session.add(save1)
+            db.session.commit()
         graphData = getGraphData()
         dates = graphData[0]
         amounts = graphData[1]
@@ -148,14 +152,14 @@ def history(usrname):
 
 @app.route("/<usrname>_goal", methods=['GET', 'POST'])
 def goal(usrname):
-	money_amount = 1
-	if request.method == 'POST':
-		money_amount = Users.query.filter_by(username=usrname).first().s_progress
-	
-	var_goal = Users.query.filter_by(username=usrname).first().s_goal
-	var_progress = Users.query.filter_by(username=usrname).first().s_progress
-	var_percent = format((var_progress / var_goal) * 100, ".2f")
-	return render_template('goal.html', var_goal=var_goal, var_progress=var_progress, var_percent=var_percent, cash=money_amount)
+    money_amount = 1
+    if request.method == 'POST':
+        money_amount = Users.query.filter_by(username=usrname).first().s_progress
+    
+    var_goal = Users.query.filter_by(username=usrname).first().s_goal
+    var_progress = Users.query.filter_by(username=usrname).first().s_progress
+    var_percent = format((var_progress / var_goal) * 100, ".2f")
+    return render_template('goal.html', var_goal=var_goal, var_progress=var_progress, var_percent=var_percent, cash=money_amount)
 
 @app.route("/logout_<usrname>")
 def logout(usrname):
@@ -175,27 +179,27 @@ def view_history():
 
 @app.route('/api/goals/<action>/<usrname>/<money>', methods=('GET', 'POST'))
 def APIgoals(action=0, usrname=0, money=0):
-	if action == 'update_goal':
-		# print(usrname, action, money)
-		found_user = Users.query.filter_by(username=usrname).first()
-		if found_user:
-			found_user.s_goal = int(money)
-			db.session.commit()
-	if action == 'update_progress':
-		# print(usrname, action, money)
-		found_user = Users.query.filter_by(username=usrname).first()
-		if found_user:
-			found_user.s_progress = found_user.s_progress + int(money)
-			db.session.commit()
-	if action == 'reset':
-		# print(usrname, action, money)
-		found_user = Users.query.filter_by(username=usrname).first()
-		if found_user:
-			found_user.s_progress = 0
-			found_user.s_goal = 1
-			db.session.commit()
+    if action == 'update_goal':
+        # print(usrname, action, money)
+        found_user = Users.query.filter_by(username=usrname).first()
+        if found_user:
+            found_user.s_goal = int(money)
+            db.session.commit()
+    if action == 'update_progress':
+        # print(usrname, action, money)
+        found_user = Users.query.filter_by(username=usrname).first()
+        if found_user:
+            found_user.s_progress = found_user.s_progress + int(money)
+            db.session.commit()
+    if action == 'reset':
+        # print(usrname, action, money)
+        found_user = Users.query.filter_by(username=usrname).first()
+        if found_user:
+            found_user.s_progress = 0
+            found_user.s_goal = 1
+            db.session.commit()
 
-	return "dummy text so the server doesn't have a fit"
+    return "dummy text so the server doesn't have a fit"
 
 
 if __name__ == '__main__':
